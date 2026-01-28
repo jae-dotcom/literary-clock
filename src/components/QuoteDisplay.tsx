ca78c81ec450642cd54d020ac3ae5b20e9a8f0a2
jae-dotcom <pi.jae.26@gmail.com>
2026-01-27 20:28:48 -0500

    quote converter

--- src/components/QuoteDisplay.tsx
+++ src/components/QuoteDisplay.tsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react'
 import { TimeQuote, QUOTES } from '../constants/TimeQuotes'
 
 
-const QuoteDisplay = () => {
+const QuoteDisplay: React.FC = () => {
   const [quote, setQuote] = useState<TimeQuote | null>(null)
 
   useEffect(() => {
     const updateQuote = () => {
       const now = new Date()
-      const currentHour = now.getHours()
-      const selectedQuote = QUOTES.find((q) => q.hour === currentHour)
+      const hh = String(now.getHours()).padStart(2, '0')
+      const mm = String(now.getMinutes()).padStart(2, '0')
+      const key = `${hh}:${mm}`
+      const selectedQuote = QUOTES.find((q) => q.time === key)
       if (selectedQuote) {
         setQuote(selectedQuote)
+      } else {
+        setQuote(null)
       }
     }
 
     updateQuote()
-    const interval = setInterval(updateQuote, 60000)
+    const interval = setInterval(updateQuote, 1000)
     return () => clearInterval(interval)
   }, [])
 
